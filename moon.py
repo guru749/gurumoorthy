b658b26ffc7eba8b79e87128c1a89bfc52293526
minthara1994 <ravi004452@gmail.com>
2020-06-17 20:56:38 -0400

    newfile

---
+++ moon.py
@@ -0,0 +1,9 @@
+>>> a.__str__
+<method-wrapper '__str__' of str object at 0x037C5BB0>
+>>> a.__str__()
+'string ramu'
+>>> str1 = "          this is a test                "
+>>> str1
+'          this is a test                '
+>>> str1.strip()
+'this is a test'
